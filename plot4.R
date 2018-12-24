bd68079d754930c90241f58f0713c993999da869
Rahul Motipalle <ramsairahul.m16@iiits.in>
2018-12-25 00:57:28 +0530

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -12,13 +12,25 @@ dev.set(which = 3)
 
 datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subdata$Global_active_power)
+globalReactivePower <- as.numeric(subdata$Global_reactive_power)
+voltage <- as.numeric(subdata$Voltage)
 subMetering1 <- as.numeric(subdata$Sub_metering_1)
 subMetering2 <- as.numeric(subdata$Sub_metering_2)
 subMetering3 <- as.numeric(subdata$Sub_metering_3)
 
-png("plot3.png", width=480, height=480)
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
 dev.off()
